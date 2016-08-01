5ff1305406c9083c38da0c032f1a302d592b9996
Marcus Klein <marcus.klein@open-xchange.com>
2016-08-01 12:11:43 +0200

    Started to implement the shop products query

--- bricklink/src/main/java/org/kleini/bricklink/api/MetaParser.java
+++ bricklink/src/main/java/org/kleini/bricklink/api/MetaParser.java
@@ -5,26 +5,12 @@
 package org.kleini.bricklink.api;
 
 import org.kleini.api.Parser;
-import org.kleini.bricklink.data.Response;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 /**
  * {@link MetaParser}
  *
- * @author <a href="mailto:marcus.klein@open-xchange.com">Marcus Klein</a>
+ * @author <a href="mailto:himself@kleini.org">Marcus Klein</a>
  */
-public class MetaParser<T extends MetaResponse<?>, U> extends Parser<T, org.kleini.bricklink.data.Response<U>> {
-
-    @Override
-    protected TypeReference<Response<U>> getResponseType() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    protected T createResponse(Response<U> response) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+public abstract class MetaParser<T extends MetaResponse<?>, U> extends Parser<T, org.kleini.bricklink.data.Response<U>> {
+    // Nothing to do.
 }
