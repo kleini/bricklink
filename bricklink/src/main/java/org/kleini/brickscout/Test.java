0fb052f812f139b15cd4b59b3baf92fb222f3612
Marcus Klein <marcus.klein@open-xchange.com>
2016-08-10 22:15:53 +0200

    started to create a spring client application

--- bricklink/src/main/java/org/kleini/brickscout/Test.java
+++ bricklink/src/main/java/org/kleini/brickscout/Test.java
@@ -1,21 +1,36 @@
+/*
+ * GPL v3
+ */
+
 package org.kleini.brickscout;
 
-import org.kleini.bricklink.api.Configuration;
-import org.kleini.brickscout.api.BrickScoutClient;
+import org.kleini.brickscout.data.Representations;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
-public class Test {
+@SpringBootApplication
+public class Test implements org.springframework.boot.CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
-        Configuration configuration = new Configuration();
-        try (
-            BrickScoutClient client = new BrickScoutClient(configuration);
-        ) {
-
-        }
+        SpringApplication.run(Test.class);
+//        Configuration configuration = new Configuration();
+//        try (
+//            BrickScoutClient client = new BrickScoutClient(configuration);
+//        ) {
+//
+//        }
         // http://docs.spring.io/spring-hateoas/docs/current/reference/html/#client
 //        Traverson traverson = new Traverson(new URI("http://localhost:8080/api/"), MediaTypes.HAL_JSON);
 //        String name = traverson.follow("movies", "movie", "actor").
 //          withTemplateParameters(parameters).
 //          toObject("$.name");
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Representations<?> representations = restTemplate.getForObject("https://api.brickscout.com/api/shops/kleini/products", Representations.class);
+        System.out.println(representations.toString());
     }
 }
