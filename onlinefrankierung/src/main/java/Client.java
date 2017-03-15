48f1c3319cf9492704eac922e060b34547f9327a
Marcus Klein <marcus.klein@open-xchange.com>
2017-03-15 23:42:29 +0100

    WSDL import is now fully integrated into Gradle build with its own configuration

---
+++ onlinefrankierung/src/main/java/Client.java
@@ -0,0 +1,13 @@
+import de.dhl.onlinefrankierung.webservice.PopPartnerService;
+
+public class Client {
+
+    public Client() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        PopPartnerService service = new PopPartnerService();
+    }
+
+}
