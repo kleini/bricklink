108f3622dabb0a680f0ca87ccf4cbdcd0bd5aa06
Marcus Klein <marcus.klein@open-xchange.com>
2017-03-16 11:09:49 +0100

    Removed generated source

--- onlinefrankierung/src/main/generated/de/dhl/onlinefrankierung/webservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://www.dhl.de/popweb/gw/ws/schema/1.0/popsc", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.dhl.onlinefrankierung.webservice;
