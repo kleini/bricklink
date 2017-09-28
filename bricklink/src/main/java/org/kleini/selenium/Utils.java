ccab3776ef0d3504a48c931788f3ca16e018e08a
Marcus Klein <himself@kleini.org>
2017-09-28 22:53:44 +0200

    fixes on XSD for BrickStock and add scrollTo method for Selenium and Chrome to solve Element not clickable at point issues

--- bricklink/src/main/java/org/kleini/selenium/Utils.java
+++ bricklink/src/main/java/org/kleini/selenium/Utils.java
@@ -5,6 +5,8 @@
 package org.kleini.selenium;
 
 import java.util.concurrent.Callable;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -18,12 +20,17 @@ public class Utils {
         super();
     }
 
-    public static void tenaciousClick(WebElement element) throws Exception {
-        retry(new Callable<Void>() {
+    public static boolean tenaciousClick(WebElement element) throws Exception {
+        return retry(new Callable<Boolean>() {
             @Override
-            public Void call() throws Exception {
-                element.click();
-                return null;
+            public Boolean call() throws Exception {
+                try {
+                    element.click();
+                    return true;
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                    throw e;
+                }
             }
         }, new Exception("Element " + element + " could not be clicked even after 10 tries."));
     }
@@ -35,17 +42,29 @@ public class Utils {
     public static <V> V retry(Callable<V> callable, Exception e) throws Exception {
         int tries = 10;
         Exception toThrow = e;
-        do {
+        while (tries > 0) {
             try {
                 return callable.call();
             } catch (Exception e1) {
+                e1.printStackTrace(System.out);
                 if (null == toThrow) {
                     toThrow = e1;
+                } else if (null == toThrow.getCause()) {
+                    toThrow.initCause(e1);
                 }
                 tries--;
                 Thread.sleep(200);
             }
-        } while (tries > 0);
+        }
         throw toThrow;
+    }
+
+    public static WebElement scrollTo(WebDriver driver, WebElement e) throws Exception {
+        if (driver instanceof JavascriptExecutor) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].scrollIntoView(false);", e);
+            return e;
+        }
+        throw new Exception("WebDriver is no Javascript executor.");
     }
 }
