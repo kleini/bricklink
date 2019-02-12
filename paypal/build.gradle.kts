dc8964ee7596068da2679dd3ac78b54e3a70d120
Marcus Klein <marcus.klein@open-xchange.com>
2019-02-12 17:14:00 +0100

    translated all JavaExec tasks

--- paypal/build.gradle.kts
+++ paypal/build.gradle.kts
@@ -1,6 +1,12 @@
 version = "0.1.0"
 
 dependencies {
+    compile("org.seleniumhq.selenium", "selenium-java", "3.6.0") {
+        exclude("org.seleniumhq.selenium", "selenium-ie-driver")
+        exclude("org.seleniumhq.selenium", "selenium-safari-driver")
+        exclude("org.seleniumhq.selenium", "selenium-opera-driver")
+        exclude("org.seleniumhq.selenium", "selenium-edge-driver")
+    }
     compile(group = "com.github.detro", name = "ghostdriver", version = "2.1.0") {
         repositories {
             maven {
@@ -8,5 +14,4 @@ dependencies {
             }
         }
     }
-    compile(project(":bricklink"))
 }
